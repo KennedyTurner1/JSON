b14bf92f09639ff49554a36c5f1e0088a8d4c769
John Doe <johndoe@example.com>
2020-09-29 14:22:30 -0500

    json 9_1

---
+++ september_9_1_map.py
@@ -0,0 +1,42 @@
+import json
+
+infile  = open('US_fires_9_1.json', 'r')
+
+fire_data = json.load(infile)
+
+lats, lons, brights = [], [], []
+
+#print(fire_data)
+
+for fire in fire_data: #for dictionary in list fire_data
+    bright = float(fire['brightness'])
+    lat = float(fire['latitude'])
+    lon = float(fire['longitude'])
+    if bright > 450:
+        brights.append(bright)
+        lats.append(lat)
+        lons.append(lon)
+
+#create the geo map 
+
+from plotly.graph_objs import Scattergeo, Layout #uses world maps
+from plotly import offline
+
+data =[{
+    'type': 'scattergeo', 
+    'lon': lons,
+    'lat': lats, 
+    'marker': {
+        'size': [1/25*bright for bright in brights],  #for each fire in the list of fires over 450, take the size of the fire and multiply by 5
+        'color': brights, 
+        'colorscale': 'Viridis', 
+        'reversescale': True, 
+        'colorbar': {'title':'Brightness'}
+    }
+}]
+
+my_layout = Layout(title='2020 Fires between September 1-13')
+
+fig = {'data': data, 'layout':my_layout}
+
+offline.plot(fig, filename='US_Fires_9_1.html')
